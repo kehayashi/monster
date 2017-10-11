3c42559001d94869e7e899b95bac9285f93d4629
kehayashi <kehayashi@hotmail.com>
2017-10-11 03:00:38 -0300

    div row progress bar

--- resources/views/home.php
+++ resources/views/home.php
@@ -430,9 +430,13 @@
                 </div>
             </div>
         </div>
-        <div class="progress">
-            <div class="progress-bar progress-bar-warning  progress-bar-striped" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 75%">
-                <span class="sr-only">75% Completo</span>
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2">
+                <div class="progress">
+                    <div class="progress-bar progress-bar-warning  progress-bar-striped" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 75%">
+                        <span class="sr-only">75% Completo</span>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
